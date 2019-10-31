4e1ae12b14e334cb890ee9ac7b91bac8aa493f95
Alejandro Guevara <alexg@auctionedge.com>
2019-10-31 16:29:13 -0600

    Add Dark Mode to title bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar app :dark="darkMode">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Alejandro Guevara</span>
+        <span class="font-weight-light">Web Developer</span>
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn icon @click="darkMode = !darkMode">
+        <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   data: () => ({
-    //
+    darkMode: false,
   }),
 };
 </script>
