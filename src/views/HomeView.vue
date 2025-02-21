80d030e22c0d1c28dbe34f4c098d576549f2ca11
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2025-02-21 19:46:55 +0100

    update the main view text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,10 @@
     <div class="hero is-fullheight-with-navbar">
       <div class="hero-body">
         <div class="block">
-          <div class="title">Hi!, I'm Alejandro Guevara, a software engineer</div>
+          <div class="title">Hi! I'm Alejandro,</div>
           <div class="block is-size-4">
             <p>
-              I work as a fullstack engineer at
+              I am a software engineer. I work as a fullstack developer at
               <a href="https://demodesk.com/" target="_blank">Demodesk</a>.
             </p>
             <p>I have long experience with Ruby on Rails and VueJS. Currently learning Go...</p>
