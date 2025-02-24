7337d37360e3216a9164ec92c06370379d85e0b6
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2025-02-24 19:26:39 +0100

    add internet-monitor project

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,8 +4,7 @@
       <div class="hero-body">
         <p class="title">Projects</p>
         <p class="subtitle">
-          A list of projects that I have posted on Github Pages that can be seen in action, plus
-          looked at its code in Github.
+          Some projects that I've worked on that can be explored either live and/or on Github.
         </p>
       </div>
     </section>
@@ -28,10 +27,10 @@
             </p>
           </div>
           <footer class="card-footer">
-            <p class="card-footer-item">
+            <p v-if="project.webpageLink" class="card-footer-item">
               <span> Project is live <a :href="project.webpageLink" target="_blank">here</a> </span>
             </p>
-            <p class="card-footer-item">
+            <p v-if="project.githubLink" class="card-footer-item">
               <span> View on <a :href="project.githubLink" target="_blank">Github</a> </span>
             </p>
           </footer>
@@ -52,13 +51,23 @@ type ProjectType = {
 
 const projects: ProjectType[] = [
   {
+    title: 'Internet monitor',
+    imageFilePath: './projects_images/internetMonitor-4by3.png',
+    lang: 'en',
+    githubLink: 'https://github.com/alejandroge/internet-monitor',
+    paragraphs: [
+      'A simple monitor for disconnections. A minimal web pages shows the recent logs',
+      'A Ruby script analyzes the results of a ping command. A minimal Go api exposes the information to a VueJS frontend.'
+    ]
+  },
+  {
     title: 'My personal website',
     imageFilePath: './projects_images/bio-4by3.png',
     lang: 'en',
     webpageLink: 'https://aguevara.me/',
     githubLink: 'https://github.com/alejandroge/bio_vuejs',
     paragraphs: [
-      'A minimal website that a use as my public portfolio. It displays Markdown articles, and some of my side projects',
+      'This website that you are on! I use it as my public portfolio to display short articles that I wrote, and some of my side projects',
       'I used VueJS and BulmaCSS. Project auto deploys to Github pages using Github Actions.'
     ]
   },
